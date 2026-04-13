8ec8f72b8e2c783813ca89cab7255fbfc8b42247
onur karaer <okaraer@my.yearupunited.org>
2026-04-13 14:46:33 -0500

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 Sakila database seems like storing data about actors and the details about the films they produced. Details are their address, country, language, payment and many more. 
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 rows
+
+SELECT * FROM film; -- Retrieved 1000 records
