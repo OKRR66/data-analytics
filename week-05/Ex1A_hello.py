28122cf67c56566fad13dfadc468b3ace0ac2383
OKRR66 <onur.karaer@icloud.com>
2026-05-06 12:15:49 -0500

    Ex1A Add initial Python script for printing messages and displaying currency

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,20 @@
 print('Hello world!')
+
+message = ('Hello world')
+print(message)
+#The message is printed twice because it executed the whole script. There are two print commands so I see the message twice.
+
+
+#Displaying dollars and cents
+
+dollars = 3
+cents = .50
+print(dollars+cents)
+#The result seem as 3.5 because float .50000000000 equals to .5
+
+cents = cents + .25
+print(dollars+cents)
+
+d_str = "3 dollars"
+c_str =  "50 cents"
+print(d_str + " " + c_str)
