2eaf5eec9d6ab80daf12ca1bce991c82ae6c254f
OKRR66 <onur.karaer@icloud.com>
2026-05-06 12:38:42 -0500

    Ex1B Add variable examples and reserved words documentation in Python script

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,34 @@
+customer_id = 12345 # Unique identifier for the customer should be an integer
+customer_name = "John Doe" # Full name of the customer should be a string
+customer_gender = "Male" # Gender of the customer should be a string 
+customer_birthday = "1990-01-01" # Date of birth of the customer should be in the format "YYYY-MM-DD"
+driver_license = "DL123456" # Driver's license number of the customer should be a string
+auto_policy_number = "AP123456" # Auto insurance policy number of the customer should be a string
+
+my_name = "Onur"
+my_lastname = "Karaer"
+# a) What is the full list of reserved words that can’t be used for variable names?
+# False	def	if	raise
+#None	del	import	return
+#True	elif	in	try
+#and	else	is	while
+#as	except	lambda	with
+#assert	finally	nonlocal	yield
+#break	for	not
+#class	form	or
+#continue	global	pass
+
+#None 
+    # is a reserved word in Python that represents the absence of a value or a null value. 
+    #It is often used to indicate that a variable has no value or to signify the end of a list or other data structure.
+    
+#elif is used in conditional statements to specify an additional condition to check if the previous conditions were not met.
+
+#lambda is a reserved word in Python that is used to create anonymous functions, which are functions that do not have a name.
+
+#return is a reserved word in Python that is used to exit a function and return a value to the caller. 
+    # It is used to specify the output of a function and can be used to return any type of data, including numbers, strings, lists, and more.
+
+#class is a reserved word in Python that is used to define a new class. 
+    # A class is a blueprint for creating objects, which are instances of the class.
+    
