4521b8a2470a511d666dc86c9e4c921e1db9df3d
OKRR66 <onur.karaer@icloud.com>
2026-05-08 14:00:36 -0500

    Ex2A Added area of a circle script

---
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -0,0 +1,10 @@
+#area_of_circle
+#a) The diameter of a given circle is the same as the day of your birthday (not the month, just the day). 
+# Figure out the formula, refresh your recollection of the difference between diameter and radius, and figure out what the script should look like.
+Diameter = 13
+Radius = Diameter / 2
+Area = 3.14 * (Radius ** 2)
+
+#b) Create the script in a file named area_of_circle.py
+#c) The displayed output should be formatted as follows: The area of a circle with radius [number] is [number]
+print(f"The area of a circle with radius {Radius} is {Area}")
