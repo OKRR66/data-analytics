547c44c8454a714dfc23a45a966a4ef6c519fa70
OKRR66 <onur.karaer@icloud.com>
2026-05-08 13:25:07 -0500

    Ex2A area_of_rectangle

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,16 @@
+#area_of_rectangle
+#2. How do you calculate the area of a rectangle?
+# Multiply the length of one side by the length of the other side (Area = Side A * Side B)
+#a) Say you have a rectangle that has dimensions corresponding to your birthday 
+# – the month number is one side and the day of the month is the other side. How would you calculate the area of this rectangle?
+Side_A = 5  
+Side_B = 13
+#My birthday is next week hehehe!
+Area = Side_A * Side_B
+#b) Figure out the formula and what the script would look like, and create the script in a file named area_of_rectangle.py
+Area = Side_A * Side_B
+#c) The displayed output should be formatted as follows: Side A is [number] Side B is [number] The area of the rectangle is [number]
+print(f"Side A is {Side_A}")
+print(f"Side B is {Side_B}")
+print(f"The area of the rectangle is {Area}")
+#d) Once your script is working, save and commit your changes.
