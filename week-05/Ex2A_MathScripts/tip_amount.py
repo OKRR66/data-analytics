30636981c77adbc9a2bbe10d92a55d393f21ba84
OKRR66 <onur.karaer@icloud.com>
2026-05-08 13:57:48 -0500

    Ex2A tip_amount script added

---
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -0,0 +1,13 @@
+#tip_amount
+#3. How do you calculate the tip amount on a restaurant bill given the tip percentage?
+#Tip = Bill Amount * Tip Percentage
+#a) Figure out the formula and what the script would look like, making up example values as needed. (If you need inspiration, what was your approximate restaurant 
+#bill the last time you ate at a restaurant?)
+tip_percentage = 0.2 
+bill_amount = 50
+tip_amount = bill_amount * tip_percentage
+#b) Create the script in a file named tip_amount.py
+
+#c) The displayed output should be formatted as follows: The tip on a $[number] restaurant bill is $[number]
+print(f"The tip on a ${bill_amount} restaurant bill is ${tip_amount}")
+#d) Once your script is working, save and commit your changes.
